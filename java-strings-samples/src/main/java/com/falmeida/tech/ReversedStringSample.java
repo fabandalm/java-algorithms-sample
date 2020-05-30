a4d2c4fbf57d411eaf5d676cc707bb27087e6128
Almeida <falmeida.tech@gmail.com>
2020-05-30 16:18:58 -0400

    Added a reverse string function.

---
+++ java-strings-samples/src/main/java/com/falmeida/tech/ReversedStringSample.java
@@ -0,0 +1,21 @@
+package com.falmeida.tech;
+
+public class ReversedStringSample {
+
+	// Reverse a String recursively
+	void reverse(String str) {
+		if ((str == null) || (str.length() <= 1))
+			System.out.println(str);
+		else {
+			System.out.print(str.charAt(str.length() - 1));
+			reverse(str.substring(0, str.length() - 1));
+		}
+	}
+
+	public static void main(String[] args) {
+		String str = "Geeks for Geeks";
+		ReversedStringSample obj = new ReversedStringSample();
+		obj.reverse(str);
+	}
+
+}
