4599f8387df84e2920c61922e0ba8660b7dd42b3
Almeida <falmeida.tech@gmail.com>
2020-06-01 13:12:23 -0400

    Added a Binary Search Tree sample.

---
+++ java-tree-samples/src/main/java/com/falmeida/tech/binary_tree/BInarySearchTreeSample.java
@@ -0,0 +1,20 @@
+package com.falmeida.tech.binary_tree;
+
+public class BInarySearchTreeSample {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	static class Node { 
+		
+        int key; 
+        Node left, right; 
+  
+        public Node(int item) { 
+            key = item; 
+            left = right = null; 
+        } 
+    } 
+
+}
