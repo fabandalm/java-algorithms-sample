65ee1083c65f2648f923763189836fe396057f71
Almeida <falmeida.tech@gmail.com>
2020-06-09 23:00:31 -0400

    Added directed graph.

--- java-graph-samples/src/main/java/com/falmeida/tech/graph/example01/DigraphSample.java
+++ java-graph-samples/src/main/java/com/falmeida/tech/graph/example01/DigraphSample.java
@@ -5,49 +5,44 @@ import java.util.Arrays;
 import java.util.List;
 
 public class DigraphSample {
-	
-	// A list of lists  to represent adjacency list
+
+	// A list of lists to represent adjacency list
 	List<List<Integer>> adj = new ArrayList<>();
 
 	// data structure to store graph edges
 	static class Edge {
 		int src, dest;
 
-		Edge(int src, int dest)
-		{
+		Edge(int src, int dest) {
 			this.src = src;
 			this.dest = dest;
 		}
 	}
-	
+
 	// Constructor to construct graph
-	public DigraphSample(List<Edge> edges)
-	{
+	public DigraphSample(List<Edge> edges) {
 		// allocate memory for adjacency list
 		for (int i = 0; i < edges.size(); i++)
 			adj.add(i, new ArrayList<>());
 
 		// add edges to the undirected graph
-		for (Edge current : edges)
-		{
+		for (Edge current : edges) {
 			// allocate new node in adjacency List from src to dest
 			adj.get(current.src).add(current.dest);
 
 			// Uncomment line 38 for undirected graph
-	
+
 			// allocate new node in adjacency List from dest to src
 			// adj.get(current.dest).add(current.src);
 		}
 	}
-	
+
 	// print adjacency list representation of graph
-	private static void printGraph(DigraphSample graph)
-	{
+	private static void printGraph(DigraphSample graph) {
 		int src = 0;
 		int n = graph.adj.size();
 
-		while (src < n)
-		{
+		while (src < n) {
 			// print current vertex and all its neighboring vertices
 			for (int dest : graph.adj.get(src))
 				System.out.print("(" + src + " --> " + dest + ")\t");
@@ -56,14 +51,13 @@ public class DigraphSample {
 			src++;
 		}
 	}
-	
+
 	// Directed Graph Implementation in Java
-	public static void main (String[] args){
-		
+	public static void main(String[] args) {
+
 		// Input: List of edges in a digraph (as per above diagram)
-		List<Edge> edges = Arrays.asList(new Edge(0, 1), new Edge(1, 2), 
-								new Edge(2, 0), new Edge(2, 1),new Edge(3, 2), 
-								new Edge(4, 5), new Edge(5, 4));
+		List<Edge> edges = Arrays.asList(new Edge(0, 1), new Edge(1, 2), new Edge(2, 0), new Edge(2, 1), new Edge(3, 2),
+				new Edge(4, 5), new Edge(5, 4));
 
 		// construct graph from given list of edges
 		DigraphSample graph = new DigraphSample(edges);
@@ -71,8 +65,5 @@ public class DigraphSample {
 		// print adjacency list representation of the graph
 		printGraph(graph);
 	}
-	
+
 }
-
-
-
